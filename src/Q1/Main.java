385410499e60cbe6d420208221b38aeead736d8e
SKT1803 <skemalt@outlook.com>
2025-05-27 21:34:05 +0300

    Update Main.java

--- src/Q1/Main.java
+++ src/Q1/Main.java
@@ -1,6 +1,6 @@
 //-----------------------------------------------------
 // Title: Main Class
-// Author: Serdar Kemal Topkaya
+// Author: SKT
 // Description: This class reads the city numbers, bidirectional roads between them, loading and running time from the console.
 // Constructs the graph by building the edges between the cities (by calculating the number of cities and edges that it should contain) 
 // and calls the method that will find the lexicographically smallest path which will take the minimum amount 
